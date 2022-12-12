63eeced93f794f9e484171c79a069ddaf56e5f6a
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-12 18:29:39 +0300

    add 075 task

---
+++ basic-javascript/075.js
@@ -0,0 +1,37 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if(strokes === 1){
+        return 'Hole-in-one!';
+    }
+
+    if(strokes >= par + 3){
+        return 'Go Home!';
+    }
+
+    if(strokes === par + 2){
+        return 'Double Bogey';
+    }
+
+    if(strokes === par + 1){
+        return 'Bogey';
+    }
+
+    if(strokes === par){
+        return 'Par';
+    }
+
+    if(strokes === par - 1){
+        return 'Birdie';
+    }
+
+    if(strokes <= par -2){
+        return 'Eagle';
+    }
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+golfScore(5, 4);
