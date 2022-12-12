551dfc600ffcb70a5e3958b714980852e4b32f3f
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-12 19:15:40 +0300

    add 083 task

---
+++ basic-javascript/083.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": 'Sharik',
+    "legs": 4,
+    "tails": 1,
+    "friends": ['other dogs', 'people']
+
+    // Only change code above this line
+};
