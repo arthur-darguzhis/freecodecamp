cdef60aa762481bc1a00f48ddf1df7ec2a601715
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-12 21:34:45 +0300

    add 092 task

---
+++ basic-javascript/092.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+];
+
+myMusic.push({
+    "artist": 'New Singer',
+    "title": 'New Album',
+    "release_year": 2022,
+    "formats": [
+        "mp3",
+        "waw"
+    ]
+})
