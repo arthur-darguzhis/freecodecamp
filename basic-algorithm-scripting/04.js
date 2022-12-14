0b6033b0c7484d21d1d22a56510dede6b8dc2e10
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-14 10:18:25 +0300

    add basic-algorithm 04 task

---
+++ basic-algorithm-scripting/04.js
@@ -0,0 +1,12 @@
+function findLongestWordLength(str) {
+    const words = str.split(' ');
+    let longestWordLength = 0;
+    for(let word of words){
+        if(word.length > longestWordLength){
+            longestWordLength = word.length;
+        }
+    }
+    return longestWordLength;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
