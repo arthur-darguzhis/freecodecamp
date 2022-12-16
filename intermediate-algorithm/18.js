10073b68cd7e77e2dcb879dde9b2062fd12461f7
arthur.darguzhis <arthur.darguzhis@gmail.com>
2022-12-16 14:52:49 +0300

    add intermediate-algorithm 18 task

---
+++ intermediate-algorithm/18.js
@@ -0,0 +1,5 @@
+function truthCheck(collection, pre) {
+    return collection.every((user) => user[pre]);
+}
+
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name");
